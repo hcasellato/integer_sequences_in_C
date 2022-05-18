0317289ffb1634d577ab6a5eac5e3bca5347de96
hcasellato <henriquecasellato@hotmail.com>
2022-05-18 19:44:52 -0300

    final code

--- recaman.c
+++ recaman.c
@@ -10,18 +10,24 @@ int main(){
 
     int maxi;
 
-    printf("Recamán's sequence\nEnter the maximum amount of numbers shown: ");
+    printf("Recamán's sequence\nEnter the maximum amount of numbers to be shown: ");
     scanf("%d", &maxi);
 
     int seq[maxi];
     seq[0] = 0;
+    printf("0 ");
 
-    for(i = 1; i <= maxi; i++){
-
-
-
+    for(int i = 1; i <= maxi; i++){
+        int a = seq[i - 1] - i;
+        for(int l = 0; l < i; l++){
+            if(a == seq[l] || a < 0){
+                a = seq[i - 1] + i;
+                break;
+            }
+        }
+        seq[i] = a;
+        printf("%d ", seq[i]);
     }
-
 
     return 0;
 }
